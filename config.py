272da8a2fd70cc55b1a01f38e5519cd19c28570a
Barnet Wang <barnet.wang@gigabyte.com>
2025-07-31 15:24:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'a-very-secret-key')
 
     # RAG Config
-    DEFAULT_MODEL = "deepseek-r1:8b" # Or any other default model you prefer
+    DEFAULT_MODEL = "qwen3:30b-a3b" # Or any other default model you prefer
     PERSIST_DIRECTORY = os.path.join(os.path.abspath(os.path.dirname(__file__)), "conversation_db")
     EMBEDDING_MODEL_NAME = "nomic-ai/nomic-embed-text-v1.5"
     OLLAMA_BASE_URL = "http://localhost:11434"
